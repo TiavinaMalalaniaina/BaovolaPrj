541367d3820496c8236aec77fef22183d7000453
randriamanantenavony <randriamanantenavony@gmail.com>
2023-12-08 14:06:17 +0500

    Update Etudiant.java

--- ProjetBaovola/src/java/etudiant/Etudiant.java
+++ ProjetBaovola/src/java/etudiant/Etudiant.java
@@ -10,5 +10,40 @@ package etudiant;
  * @author itu
  */
 public class Etudiant {
+    int idEtudiant;
+    String nom;
+    String prenom;
+
+    public int getIdEtudiant() {
+        return idEtudiant;
+    }
+
+    public void setIdEtudiant(int idEtudiant) {
+        this.idEtudiant = idEtudiant;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public Etudiant(int idEtudiant, String nom, String prenom) {
+        this.idEtudiant = idEtudiant;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    
+     
     
 }
